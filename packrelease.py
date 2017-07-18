77a3190adb4c5e2df96c39376e8613917a240039
Andreas Alexander Maier <andbot@gmx.de>
2017-07-18 14:56:48 +0200

    Add encrypted question collection

--- packrelease.py
+++ packrelease.py
@@ -8,6 +8,8 @@ parser.add_option("-p", "--public", dest="ispublic", default=True,
                   help="Include example question catalogue instead of real")
 (options, args) = parser.parse_args()
 
+
+# First pack release in unencrypted zip
 command = '7z a -tzip'
 archivename = 'Fuxenpruefung.zip'
 files = [r'"C:\Users\Andreas Maier\Dropbox\Projects\Python\Fuxenpruefung\dist\Fuxenpruefung.exe"']
@@ -20,6 +22,13 @@ if not options.ispublic:
     question_file = 'Fragensammlung.txt'
     files.append(question_file)
 allfiles = functools.reduce(lambda a, b: a + ' ' + b, files)
-
 os.system('del '+archivename)
 os.system(command+' '+archivename+' '+allfiles)
+
+# then encrypt secret question file
+archivename = 'Fragensammlung.zip'
+os.system('del '+archivename)
+print('Enter password to encrypt question file:')
+passwd = input()
+command = '7z a -tzip -p'+passwd+' '+archivename+' Fragensammlung.txt'
+os.system(command)
