72805a14e87d2b94ed5976d446cf12991d9ada0e
Andreas Alexander Maier <andbot@gmx.de>
2017-07-18 13:32:36 +0200

    Include English example in release zip

--- packrelease.py
+++ packrelease.py
@@ -12,11 +12,13 @@ command = '7z a -tzip'
 archivename = 'Fuxenpruefung.zip'
 files = [r'"C:\Users\Andreas Maier\Dropbox\Projects\Python\Fuxenpruefung\dist\Fuxenpruefung.exe"']
 question_file = ""
-if options.ispublic:
-    question_file = 'Fragensammlung_Beispiel.txt'
-else:
-    question_file = 'Fragensammlung.txt'
+question_file = 'Fragensammlung_Beispiel.txt'
 files.append(question_file)
+question_file = 'Example_questions.txt'
+files.append(question_file)
+if not options.ispublic:
+    question_file = 'Fragensammlung.txt'
+    files.append(question_file)
 allfiles = functools.reduce(lambda a, b: a + ' ' + b, files)
 
 os.system('del '+archivename)
