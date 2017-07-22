44b19d70632878566cbe356bf8020e192f937af8
Andreas Alexander Maier <andbot@gmx.de>
2017-07-22 17:22:59 +0200

    Outsource language libraries

--- src/fuxenpruefung.py
+++ src/fuxenpruefung.py
@@ -1,13 +1,15 @@
-import sys
 import os
 import random
-import tkinter as tk
-from tkinter import filedialog, simpledialog
-import webbrowser
 import subprocess
+import sys
+import webbrowser
 import zipfile
 from time import sleep
-# from i18n import I18N
+
+import tkinter as tk
+from tkinter import filedialog, simpledialog
+
+import i18n
 
 
 base_path = ''
@@ -20,100 +22,28 @@ question_file = ''
 
 
 reinit = False
-current_lang = 'ger'
-
+lang = 'ger'
 categories = []
-lg_names = {'ger': ['Kleine Frage', 'Mittlere Frage', 'Große Frage', 'Permanente Frage',
-                    'Scherzfrage', 'Archiv'],
-            'eng': ['Small question', 'Medium question', 'Hard question', 'Permanent question',
-                    'Joke question', 'Archive']}
-short_names = {'ger': ['S', 'M', 'H', 'P', 'J', 'A'],
-               'eng': ['S', 'M', 'H', 'P', 'J', 'A']}
-start_button_text = []
-start_button_texts = {'ger': ['Start', 'Schließen'],
-                      'eng': ['Go!', 'Close']}
-link_label_text = ''
-link_label_texts = {'ger': 'AGV Webseite öffnen',
-                    'eng': 'Open AGV website'}
-error_title = ''
-error_titles = {'ger': 'Fehler!',
-                'eng': 'Error!'}
-error_text = []
-error_texts = {'ger': ['Keine Fragensammlung ausgewaehlt! Nochmal!', 'Falsches Passwort! Nochmal!'],
-               'eng': ['No question file selected. Retry!', 'Bad password. Retry!']}
-dict_init = []
-dict_inits = {'ger': ['Erstelle neue Fuxenprüfung', 'Zeige Fragenstatistik', 'Zeige alle Fragen'],
-              'eng': ['Compile new exam', 'Show question statistics', 'Show all questions']}
-exam_title = []
-exam_titles = {'ger': ['Fuxenprüfung', 'Fuxenlösung'],
-               'eng': ['Exam', 'Solution']}
-statistics_header = []
-statistics_headers = {'ger': ['Fragenpool nach Schwierigkeit', 'Fragenpool nach Thema'],
-                      'eng': ['Question pool by difficulty', 'Question pool by topic']}
-statistics_colheader = []
-statistics_colheaders = {'ger': ['Kategorie', 'Anzahl', 'Anteil'],
-                         'eng': ['Category', 'Number', 'Fraction']}
-allquestions_header = ''
-allquestions_headers = {'ger': 'Alle verfügbaren Fragen und Antworten',
-                        'eng': 'All available questions and answers'}
-allquestions_colheader = []
-allquestions_colheaders = {'ger': ['Frage', 'Antwort', 'Kateg.', 'Schw.', 'Platz'],
-                           'eng': ['Question', 'Answer', 'Categ.', 'Diff.', 'Space']}
-app_header = ''
-app_headers = {'ger': 'Fuxenprüfungsgenerator',
-               'eng': 'Exam generator'}
-password_text = []
-password_texts = {'ger': ['Passwort', 'Datei ist verschlüsselt! Bitte Passwort eingeben:'],
-                  'eng': ['Password', 'File is encryoted! Please enter password:']}
-exam_file = []
-exam_files = {'ger': ['fuxenpruefung.txt', 'fuxenloesung.txt'],
-              'eng': ['exam.txt', 'solution.txt']}
 
 
 def setLanguage(key='ger'):
-    global current_lang
-    current_lang = key
+    global lang
+    lang = key
     global categories
     categories = [
-                  [16, lg_names[key][0], short_names[key][0]],
-                  [6, lg_names[key][1], short_names[key][1]],
-                  [4, lg_names[key][2], short_names[key][2]],
-                  [1000, lg_names[key][3], short_names[key][3]],
-                  [5, lg_names[key][4], short_names[key][4]],
-                  [0, lg_names[key][5], short_names[key][5]],
+                  [16, i18n.lg_names[lang][0], i18n.short_names[lang][0]],
+                  [6, i18n.lg_names[lang][1], i18n.short_names[lang][1]],
+                  [4, i18n.lg_names[lang][2], i18n.short_names[lang][2]],
+                  [1000, i18n.lg_names[lang][3], i18n.short_names[lang][3]],
+                  [5, i18n.lg_names[lang][4], i18n.short_names[lang][4]],
+                  [0, i18n.lg_names[lang][5], i18n.short_names[lang][5]],
                  ]
-    global start_button_text
-    start_button_text = start_button_texts[key]
-    global link_label_text
-    link_label_text = link_label_texts[key]
-    global error_title
-    error_title = error_titles[key]
-    global error_text
-    error_text = error_texts[key]
-    global dict_init
-    dict_init = dict_inits[key]
-    global exam_title
-    exam_title = exam_titles[key]
-    global statistics_header
-    statistics_header = statistics_headers[key]
-    global statistics_colheader
-    statistics_colheader = statistics_colheaders[key]
-    global allquestions_header
-    allquestions_header = allquestions_headers[key]
-    global allquestions_colheader
-    allquestions_colheader = allquestions_colheaders[key]
-    global app_header
-    app_header = app_headers[key]
-    global password_text
-    password_text = password_texts[key]
-    global exam_file
-    exam_file = exam_files[key]
 
 
 def switchLanguage():
-    if current_lang == 'ger':
+    if lang == 'ger':
         setLanguage('eng')
-    elif current_lang == 'eng':
+    elif lang == 'eng':
         setLanguage('ger')
     global reinit
     reinit = True
@@ -172,7 +102,7 @@ class InitWindow:
         self.radio_var = tk.IntVar()
         _row_count = 0
 
-        _head_label = tk.Label(master, text=app_header, font=("Helvetica", 16))
+        _head_label = tk.Label(master, text=i18n.app_header[lang], font=("Helvetica", 16))
         _head_label.grid(row=_row_count, columnspan=4)
         _row_count += 1
 
@@ -199,15 +129,17 @@ class InitWindow:
             if (_col_idx == 0):
                 _row_count += 2
 
-        for idx, task in enumerate(dict_init):
+        for idx, task in enumerate(i18n.dict_init[lang]):
             rad = tk.Radiobutton(master, text=task, variable=self.radio_var, value=idx)
             rad.grid(row=_row_count, columnspan=4)
             _row_count += 1
         self.radio_var.set(radioinit)
 
-        _start_button = tk.Button(master, text=start_button_text[0], fg="green", font="bold", command=master.quit)
+        _start_button = tk.Button(master, text=i18n.start_button_text[lang][0],
+                                  fg="green", font="bold", command=master.quit)
         _start_button.grid(row=_row_count, column=0, columnspan=2)
-        _quit_button = tk.Button(master, text=start_button_text[1], fg="red", font="bold", command=sys.exit)
+        _quit_button = tk.Button(master, text=i18n.start_button_text[lang][1],
+                                 fg="red", font="bold", command=sys.exit)
         _quit_button.grid(row=_row_count, column=2, columnspan=2)
         _row_count += 1
 
@@ -226,7 +158,7 @@ class InitWindow:
         _github_button.bind("<Button-1>", callback_GitHub)
         _github_button.grid(row=_row_count, column=3)
 
-        _link_label = tk.Label(master, text=link_label_text, fg="blue", cursor="hand2")
+        _link_label = tk.Label(master, text=i18n.link_label_text[lang], fg="blue", cursor="hand2")
         _link_label.grid(row=_row_count, column=1, columnspan=2)
         _link_label.bind("<Button-1>", callback_AGV)
 
@@ -239,7 +171,7 @@ class InitWindow:
 
         def _toggledown(self):
             old = thisradio.get()
-            new = min(old + 1, len(dict_init)-1)
+            new = min(old + 1, len(i18n.dict_init[lang])-1)
             thisradio.set(new)
 
         def _quit(self):
@@ -355,7 +287,7 @@ task_var = 0
 zip_passwd = ''
 while True:
 
-    setLanguage(current_lang)
+    setLanguage(lang)
     useGUI = True
     quest_numbers = {}
     if useGUI:
@@ -390,7 +322,8 @@ while True:
             for zinfo in zf.infolist():
                 is_encrypted = zinfo.flag_bits & 0x1
             if is_encrypted and zip_passwd == '':
-                zip_passwd = simpledialog.askstring(password_text[0], password_text[1], show='*')
+                zip_passwd = simpledialog.askstring(i18n.password_text[lang][0],
+                                                    i18n.password_text[lang][1], show='*')
                 try:
                     zip_passwd_bytes = str.encode(zip_passwd)
                 except TypeError:
@@ -420,9 +353,9 @@ while True:
             error_idx = 1
         root = tk.Tk()
         root.iconbitmap(fox_ico)
-        root.title(error_title)
+        root.title(i18n.error_title[lang])
         lines = []
-        lines.append(error_text[error_idx])
+        lines.append(i18n.error_text[lang][error_idx])
         app = InfoWindow(root, lines)
         root.mainloop()
         root.destroy()
@@ -430,7 +363,7 @@ while True:
         question_file = ''
         continue
 
-    print('Selected task:', dict_init[task_var])
+    print('Selected task:', i18n.dict_init[lang][task_var])
 
     # Read in data
     qdicts = {}
@@ -475,8 +408,8 @@ while True:
             ran_QnA = [(qdict[key][:2]+(qdict[key][3],)) for key in keys][:quest_numbers[qdict_str]]
             ran_qdicts[qdict_str] = ran_QnA
 
-        with open(exam_file[0], 'w', encoding='utf8') as myfile:
-            print(exam_title[0]+"\n", file=myfile)
+        with open(i18n.exam_file[lang][0], 'w', encoding='utf8') as myfile:
+            print(i18n.exam_title[lang][0]+"\n", file=myfile)
             count = 0
             for default, lg_name, short_name in categories:
                 for question, answer, vspace in ran_qdicts[short_name]:
@@ -487,18 +420,18 @@ while True:
                         print('\to '+item, file=myfile)
                     print('\n'*int(vspace), file=myfile)
 
-        with open(exam_file[1], 'w', encoding='utf8') as myfile:
-            print(exam_title[1]+"\n", file=myfile)
+        with open(i18n.exam_file[lang][1], 'w', encoding='utf8') as myfile:
+            print(i18n.exam_title[lang][1]+"\n", file=myfile)
             count = 0
             for default, lg_name, short_name in categories:
                 for question, answer, vspace in ran_qdicts[short_name]:
                     count += 1
                     print('{}.'.format(count), answer, file=myfile)
 
-        sys_command = 'notepad '+exam_file[1]
+        sys_command = 'notepad '+i18n.exam_file[lang][1]
         subprocess.Popen(sys_command)
         sleep(0.1)
-        sys_command = 'notepad '+exam_file[0]
+        sys_command = 'notepad '+i18n.exam_file[lang][0]
         subprocess.Popen(sys_command)
 
     elif task_var == 1:
@@ -506,14 +439,14 @@ while True:
         tot_n_questions = len(qdicts_all)
         # create message
         lines = []
-        lines.append(statistics_header[0])
-        lines.append(statistics_colheaders)
+        lines.append(i18n.statistics_header[lang][0])
+        lines.append(i18n.statistics_colheader[lang])
         for default, lg_name, short_name in categories:
             lines.append((lg_name+': ', str(len(qdicts[short_name])),
                          '{:.0f} %'.format(100*len(qdicts[short_name])/tot_n_questions)))
 
-        lines.append(statistics_header[1])
-        lines.append(statistics_colheaders)
+        lines.append(i18n.statistics_header[lang][1])
+        lines.append(i18n.statistics_colheader[lang])
         count_dict = Counter([x[2] for x in qdicts_all.values()])
         keys = list(count_dict.keys())
         keys.sort()
@@ -531,9 +464,9 @@ while True:
 
     elif task_var == 2:
 
-        header = allquestions_header
+        header = i18n.allquestions_header[lang]
         lines = []
-        lines.append(allquestions_colheader)
+        lines.append(i18n.allquestions_colheader[lang])
         for key in qdicts_all:
             lines.append((qdicts_all[key][0],
                           qdicts_all[key][1],
--- src/i18n.py
+++ src/i18n.py
@@ -1,2 +1,33 @@
-class I18N(object):
-    pass
+lg_names = {'ger': ['Kleine Frage', 'Mittlere Frage', 'Große Frage', 'Permanente Frage',
+                    'Scherzfrage', 'Archiv'],
+            'eng': ['Small question', 'Medium question', 'Hard question', 'Permanent question',
+                    'Joke question', 'Archive']}
+short_names = {'ger': ['S', 'M', 'H', 'P', 'J', 'A'],
+               'eng': ['S', 'M', 'H', 'P', 'J', 'A']}
+
+start_button_text = {'ger': ['Start', 'Schließen'],
+                     'eng': ['Go!', 'Close']}
+link_label_text = {'ger': 'AGV Webseite öffnen',
+                   'eng': 'Open AGV website'}
+error_title = {'ger': 'Fehler!',
+               'eng': 'Error!'}
+error_text = {'ger': ['Keine Fragensammlung ausgewaehlt! Nochmal!', 'Falsches Passwort! Nochmal!'],
+              'eng': ['No question file selected. Retry!', 'Bad password. Retry!']}
+dict_init = {'ger': ['Erstelle neue Fuxenprüfung', 'Zeige Fragenstatistik', 'Zeige alle Fragen'],
+             'eng': ['Compile new exam', 'Show question statistics', 'Show all questions']}
+exam_title = {'ger': ['Fuxenprüfung', 'Fuxenlösung'],
+              'eng': ['Exam', 'Solution']}
+statistics_header = {'ger': ['Fragenpool nach Schwierigkeit', 'Fragenpool nach Thema'],
+                     'eng': ['Question pool by difficulty', 'Question pool by topic']}
+statistics_colheader = {'ger': ['Kategorie', 'Anzahl', 'Anteil'],
+                        'eng': ['Category', 'Number', 'Fraction']}
+allquestions_header = {'ger': 'Alle verfügbaren Fragen und Antworten',
+                       'eng': 'All available questions and answers'}
+allquestions_colheader = {'ger': ['Frage', 'Antwort', 'Kateg.', 'Schw.', 'Platz'],
+                          'eng': ['Question', 'Answer', 'Categ.', 'Diff.', 'Space']}
+app_header = {'ger': 'Fuxenprüfungsgenerator',
+              'eng': 'Exam generator'}
+password_text = {'ger': ['Passwort', 'Datei ist verschlüsselt! Bitte Passwort eingeben:'],
+                 'eng': ['Password', 'File is encryoted! Please enter password:']}
+exam_file = {'ger': ['fuxenpruefung.txt', 'fuxenloesung.txt'],
+             'eng': ['exam.txt', 'solution.txt']}
