306ce93e201effa98c2d7099d540de73562e062d
Andreas Alexander Maier <andbot@gmx.de>
2017-10-01 07:05:14 +0200

    Outsource read in of data

---
+++ src/data.py
@@ -0,0 +1,64 @@
+import os
+import sys
+
+import tkinter as tk
+
+import files
+import gui
+import i18n
+
+foxIco = files.resource_path('', r'images\fox.ico')
+
+
+def read_data(questNumbers, questionFile, zipFile, zipPasswd_bytes):
+    qdicts = {}
+    for key in questNumbers.keys():
+        qdicts[key] = {}
+    qdictsAll = {}
+
+    questCounter = 0
+    questLines = []
+    if questionFile.endswith('.zip'):
+        base = os.path.basename(questionFile)
+        with zipFile.open(os.path.splitext(base)[0]+'.txt', pwd=zipPasswd_bytes) as data:
+            for byteLine in data:
+                line = byteLine.decode('utf8')
+                questLines.append(line)
+    else:
+        with open(questionFile, 'r', encoding='utf8') as data:
+            for line in data:
+                questLines.append(line)
+
+    for line in questLines:
+        line = line.rstrip()
+        if line.startswith('#') or not len(line):
+            continue
+        splitlist = []
+        try:
+            splitlist = line.split("#", 4)
+        except ValueError:
+            continue
+        splitlist = [x for x in map(lambda a: a.strip(), splitlist)]
+        try:
+            difficulty, question, answer, category, vspace = splitlist
+        except ValueError:
+            errorIdx = 2
+            root = tk.Tk()
+            if sys.platform == 'win32':
+                root.iconbitmap(foxIco)
+            root.title(i18n.errorTitle[i18n.lang()])
+            lines = []
+            lines.append(i18n.errorText[i18n.lang()][errorIdx] + ' ' + line)
+            gui.InfoWindow(root, lines)
+            root.focus_force()
+            root.mainloop()
+            root.destroy()
+            # zipPasswd = ''
+            questionFile = ''
+            continue
+
+        qdicts[difficulty][len(qdicts[difficulty])] = question, answer, category, vspace
+        qdictsAll[questCounter] = question, answer, category, difficulty, vspace
+        questCounter += 1
+
+    return qdicts, qdictsAll
--- src/fuxenpruefung.py
+++ src/fuxenpruefung.py
@@ -5,6 +5,7 @@ import zipfile
 import tkinter as tk
 from tkinter import filedialog, simpledialog
 
+import data
 import i18n
 import gui
 import files
@@ -81,8 +82,8 @@ while True:
 
     passwordError = False
     if questionFile != () and questionFile.endswith('.zip'):
-        zf = zipfile.ZipFile(questionFile)
-        for zinfo in zf.infolist():
+        zipFile = zipfile.ZipFile(questionFile)
+        for zinfo in zipFile.infolist():
             isEncrypted = zinfo.flag_bits & 0x1
         if isEncrypted and zipPasswd == '':
             zipPasswd = simpledialog.askstring(i18n.passwordText[i18n.lang()][0],
@@ -94,11 +95,14 @@ while True:
             print(os.path.splitext(questionFile)[0])
             base = os.path.basename(questionFile)
             try:
-                with zf.open(os.path.splitext(base)[0]+'.txt', pwd=zipPasswd_bytes) as data:
+                with zipFile.open(os.path.splitext(base)[0]+'.txt', pwd=zipPasswd_bytes):
                     pass
             except RuntimeError:
                 print('Bad password!')
                 passwordError = True
+    else:
+        zipFile = ''
+        zipPasswd_bytes = b''
 
     mainroot.destroy()
 
@@ -121,56 +125,7 @@ while True:
     if not questionFile:
         continue
 
-    # Read in data
-    qdicts = {}
-    for key in questNumbers.keys():
-        qdicts[key] = {}
-    qdictsAll = {}
-
-    questCounter = 0
-    questLines = []
-    if questionFile.endswith('.zip'):
-        base = os.path.basename(questionFile)
-        with zf.open(os.path.splitext(base)[0]+'.txt', pwd=zipPasswd_bytes) as data:
-            for byteLine in data:
-                line = byteLine.decode('utf8')
-                questLines.append(line)
-    else:
-        with open(questionFile, 'r', encoding='utf8') as data:
-            for line in data:
-                questLines.append(line)
-
-    for line in questLines:
-        line = line.rstrip()
-        if line.startswith('#') or not len(line):
-            continue
-        splitlist = []
-        try:
-            splitlist = line.split("#", 4)
-        except ValueError:
-            continue
-        splitlist = [x for x in map(lambda a: a.strip(), splitlist)]
-        try:
-            difficulty, question, answer, category, vspace = splitlist
-        except ValueError:
-            errorIdx = 2
-            root = tk.Tk()
-            if sys.platform == 'win32':
-                root.iconbitmap(foxIco)
-            root.title(i18n.errorTitle[i18n.lang()])
-            lines = []
-            lines.append(i18n.errorText[i18n.lang()][errorIdx] + ' ' + line)
-            app = gui.InfoWindow(root, lines)
-            root.focus_force()
-            root.mainloop()
-            root.destroy()
-            zipPasswd = ''
-            questionFile = ''
-            continue
-
-        qdicts[difficulty][len(qdicts[difficulty])] = question, answer, category, vspace
-        qdictsAll[questCounter] = question, answer, category, difficulty, vspace
-        questCounter += 1
+    qdicts, qdictsAll = data.read_data(questNumbers, questionFile, zipFile, zipPasswd_bytes)
 
     # process tasks below
     if taskVar == 0:
