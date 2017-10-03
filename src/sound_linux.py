b7ef1ef121c75768cf9c6472aad5f40d4e8404a1
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-10-03 09:48:58 +0200

    Add linux event sounds

--- src/sound_linux.py
+++ src/sound_linux.py
@@ -24,6 +24,8 @@ stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                 output=True,
                 stream_callback=callback)
 
+stream.stop_stream()
+
 
 def sound():
     return SOUND_IS_ON
@@ -67,8 +69,28 @@ def toggle_sound(doSwitch=True):
 
 
 def play_sound(sound):
-    pass
+    try:
+        wf = wave.open(sound, 'rb')
 
+        # instantiate PyAudio (1)
+        p = pyaudio.PyAudio()
+
+        # define callback (2)
+        def callback(in_data, frame_count, time_info, status):
+            data = wf.readframes(frame_count)
+            return (data, pyaudio.paContinue)
+
+        # open stream using callback (3)
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True,
+                        stream_callback=callback)
+
+        # start the stream (4)
+        stream.start_stream()
+    except wave.Error:
+        print('Warning: caught wave.Error!')
 
 def main():
     print('Playing the sound on your system!')
